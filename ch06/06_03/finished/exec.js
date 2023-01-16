e61e22161c4a3a1e22b329c3d4f7953b432da937
donavance08 <adonisavance@gmail.com>
2023-01-16 22:17:50 +0800

    ch06-03 done

--- ch06/06_03/finished/exec.js
+++ ch06/06_03/finished/exec.js
@@ -1,5 +1,31 @@
 const cp = require("child_process");
 
-cp.exec("node readStream", (err, data, stderr) => {
-  console.log(data);
-});
+/* 
+  Node.js comes with a “child_process” module which allow you to execute external 
+  processes to your environment, which allows your Node.js app to communicate with 
+  other app w/in the environment that it's hosted.
+
+  cp.exec("open http://www.linkedin.com/learning");
+
+  cp.exec("ls", (err, data) => {
+    if (err){
+      throw err;
+    }
+
+    console.log(data)
+  })
+
+*/
+
+// cp.exec("node readStream", (err, data, stderr) => {
+//   console.log(data);
+// });
+
+
+cp.exec("ls", (err, data) => {
+  if (err){
+    throw err;
+  }
+
+  console.log(data)
+})
