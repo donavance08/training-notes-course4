8f1743af522ea31b10d70d8d007b1bf7b80f7256
donavance08 <adonisavance@gmail.com>
2023-01-16 21:52:37 +0800

    chapter 6.2 done

--- ch06/06_02/finished/writeStream.js
+++ ch06/06_02/finished/writeStream.js
@@ -1,6 +1,25 @@
 const fs = require("fs");
 
+// To create a new writeStream
 const writeStream = fs.createWriteStream("./assets/myFile.txt", "UTF-8");
 const readStream = fs.createReadStream("./assets/lorum-ipsum.md", "UTF-8");
 
+// This will write contents of the readstream directly to the writeStream output file. 
 readStream.pipe(writeStream);
+
+
+// This will write "hello" the output file "./assets/myFile.txt"
+// writeStream.write("hello")
+
+/* 
+    * To listen for user input and automatically write it to the output file
+    * Will append new input to end of file
+
+    process.stdin.on("data", data => {
+        writeStream.write(data);
+    });
+*/
+
+
+
+
